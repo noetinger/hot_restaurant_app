480159e5d052645a9dfa15a81fff6226c1a7eaa3
Nick O <nicholas.oetinger@gmail.com>
2019-07-13 11:17:30 -0400

    Added create new table logic

--- server.js
+++ server.js
@@ -7,44 +7,51 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 //Set up the Express app to handle the data parsing
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 //Table Data
-var table = [
-    {
+var table = [{
     name: "Nack",
     phone: 7045556666,
     email: "nack@email.com",
     ID: "N420"
-    }
-];
+}];
 
 //Routes
 //Basic route that sends user to the Home Page
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
 //route to reserve table page
-app.get("/reserve", function (req, res){
+app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 //route to view table page and display table
-app.get("/tables", function (req, res){
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
     return res.json(table);
 });
 
 //Create new table
+app.post("/tables", function (req, res) {
 
+    var newTable = req.body;
 
+    console.log(newTable);
 
+    table.push(newTable);
+
+    res.json(newTable);
+});
 
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
